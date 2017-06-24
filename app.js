28c699b426a2c69e5ac9f4eb737266e4a7d78a91
Vineeth Suvarna <vineeth22@live.in>
2017-06-24 17:54:00 +0530

    created authentication api

--- app.js
+++ app.js
@@ -3,31 +3,59 @@ const app = express()
 var bodyparser = require("body-parser")
 var sessions = require("client-sessions");
 var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+    , assert = require('assert');
 
 // Connection URL
 var url = 'mongodb://localhost:27017/leave-app';
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
+/*MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
-  console.log("Connected successfully to server");
-
   db.close();
 });
-
+*/
 app.use(sessions({
-  cookieName: 'session', // cookie name dictates the key name added to the request object
-  secret: 'blargadeeblargblarg', // should be a large unguessable string
-  duration: 30 * 60 * 1000, // how long the session will stay valid in ms
-  activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+    cookieName: 'session', // cookie name dictates the key name added to the request object
+    secret: 'blargadeeblargblarg', // should be a large unguessable string
+    duration: 30 * 60 * 1000, // how long the session will stay valid in ms
+    activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
 }));
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json({ type: "application/json" }));
 
+
+
 app.use(express.static('public'))
+/*  
+    "/login" is a post request used for initial authentication and 
+    to set session if successfully authenticated
+    "/login" takes json data in the form of
+    { username: 'user', role: 'manager' }
+    response in the form of {result:"successful"}/{result:"unsuccessful"}
+*/
+app.post("/login", function (req, res) {    
+    var login = req.body;
+    var obj = new Object();
+    MongoClient.connect(url, function (err, db) {
+        assert.equal(null, err);
+        db.collection("user").find(login).toArray(function (err, r) { //querying the database
+            if (r.length == 0) { 
+                obj.result = "unsuccessful";
+                res.send(obj);
+            }
+            else {
+                req.session.username = login.username;  //setting session
+                req.session.role = login.role;
+                obj.result = "successful";
+                res.send(obj);
+            }
+
+        })
+        db.close();
+    });
+})
 
 app.listen(3000, function () {
-  console.log('App listening on port 3000')
+    console.log('App listening on port 3000')
 })
